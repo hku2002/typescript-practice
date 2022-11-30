c316719ed5c727793c53c548e858ff91f39520f1
Hwang <hku2002@hanmail.net>
2022-11-30 23:50:36 +0900

    feat: 함수 연습 추가

--- samples/firstPractice.js
+++ samples/firstPractice.js
@@ -1,2 +1,13 @@
 "use strict";
 let sample = 'test';
+const sampleNumber = 10;
+const sampleString = '10';
+const sampleBoolean = true;
+const sampleUndefined = undefined;
+const sampleNull = null;
+/* 기본 함수 샘플 */
+function add(x, y) { return x + y; }
+;
+/* 화살표 함수 사용 */
+const addArrow = (a, b) => a + b;
+const addTypeAndArrow = (i, j) => i + j;
--- samples/firstPractice.ts
+++ samples/firstPractice.ts
@@ -1,3 +1,19 @@
 "use strict";
 
 let sample = 'test';
+
+const sampleNumber: number = 10;
+const sampleString: string = '10';
+const sampleBoolean: boolean = true;
+const sampleUndefined: undefined = undefined;
+const sampleNull: null = null;
+
+/* 기본 함수 샘플 */
+function add(x: number, y: number): number { return x + y };
+
+/* 화살표 함수 사용 */
+const addArrow: (a: number, b: number) => number = (a, b) => a + b;
+
+/* type 으로 선언 후 화살표 함수 사용 */
+type addType = (i: number, j: number) => number;
+const addTypeAndArrow: addType = (i, j) => i + j;
